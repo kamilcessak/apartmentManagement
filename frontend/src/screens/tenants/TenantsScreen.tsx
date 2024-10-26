050ea748059d6742f02dff03b5ae35d17ebe8754
Kamil Cessak <kamil.cessak@mindconsulting.pl>
2024-10-26 11:48:14 +0200

    added loading and error views

--- frontend/src/screens/tenants/TenantsScreen.tsx
+++ frontend/src/screens/tenants/TenantsScreen.tsx
@@ -1,24 +1,28 @@
-import { useEffect } from "react";
 import axios from "axios";
 import { RouteContent } from "../components/common";
+import { useQuery } from "@tanstack/react-query";
 
 export const TenantsScreen = () => {
   const getTenants = async () => {
     try {
       const response = await axios.get("http://localhost:5050/tenants");
-      console.log({ response });
+      return response.data;
     } catch (error) {
       console.error(error);
     }
   };
 
-  useEffect(() => {
-    void getTenants();
-  }, []);
+  const { data } = useQuery({
+    queryFn: getTenants,
+    queryKey: ["tenants", "list"],
+  });
+
+  console.log({ data });
 
   return (
     <RouteContent>
       <h1>List of your tenants:</h1>
+      <div>{data?.map((tenant) => <div key={tenant.id}></div>)}</div>
     </RouteContent>
   );
 };
